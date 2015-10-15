6388b65cefb4ccf5620bbd7f45e29a2f28b7dd3d
hecoding <hlaria@ucm.es>
2015-10-15 02:42:16 +0200

    un par de funciones útiles

--- modlist.c
+++ modlist.c
@@ -8,17 +8,14 @@
 
 MODULE_LICENSE("GPL");
 
-#define BUFFER_LENGTH       PAGE_SIZE
+#define BUFFER_LENGTH PAGE_SIZE
 
-struct list_head mylist;
-/* Lista enlazada */
+struct list_head mylist; /* Lista enlazada */
 /* Nodos de la lista */
 typedef struct {
 	int data;
 	struct list_head links;
 } list_item_t;
-
-
 
 static struct proc_dir_entry *proc_entry;
 static char *modlist;  // Space for the "modlist"
@@ -57,12 +54,48 @@ int init_modlist_module( void )
 
 }
 
-
 void exit_modlist_module( void )
 {
   remove_proc_entry("modlist", NULL);
   vfree(modlist);
   printk(KERN_INFO "modlist: Module unloaded.\n");
+}
+
+
+static int modlist_read(/* parametros */) {
+
+}
+
+static int modlist_write(/* parametros */) {
+
+}
+
+static int modlist_add(/* parametros */) {
+
+}
+
+static int modlist_remove(/* parametros */) {
+
+}
+
+static int modlist_cleanup(/* parametros */) {
+
+}
+
+void print_list(struct list_head* list) {
+  struct list_item_t* item = NULL;
+  struct list_head* cur_node = NULL;
+  list_for_each(cur_node, list) {
+  /* item points to the structure wherein the links are embedded */
+    item = list_entry(cur_node, struct list_item, links);
+    printk(KERN_INFO "%i \n", item->data);
+  }
+}
+
+void f(void) {
+  INIT_LIST_HEAD(&my_list); /* Initialize the list */
+  //do_something(&my_list); /* Populate the list */
+  print_list(&my_list);
 }
 
 
--- pruebas.c
+++ pruebas.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "list.h"
 
-struct list_head mylist;
 /* Lista enlazada */
+struct list_head mylist;
 /* Nodos de la lista */
 typedef struct {
 	int data;
@@ -10,16 +10,47 @@ typedef struct {
 } list_item_t;
 
 int main() {
-	printf("afa\n");
-	INIT_LIST_HEAD(mylist);
-	list_item_t* ptr;
-	int i=0;
-
-	for (i=0;i<20;i++){
-		ptr=malloc(sizeof(list_item_t));
-		list_add_tail(&ptr->links,&mylist);
-	}	
+	blah();
 	 
 
 	return 0;
+}
+
+void populate(struct list_head* list) {
+	list_item_t* ptr;
+	int i=0;
+	int NUM = 20;
+
+	for (i=0;i<NUM;i++){
+		ptr=malloc(sizeof(list_item_t));
+		ptr->data = i;
+		list_add_tail(&ptr->links,&mylist);
+	}
+}
+
+void introducir_profesor(struct list_head* list) {
+	list_item_t* ptr;
+	int i=0;
+	int NUM = 20;
+
+	for (i=0;i<NUM;i++){
+		ptr=malloc(sizeof(list_item_t));
+		list_add_tail(&ptr->links,&mylist);
+	}
+}
+
+void print_list(struct list_head* list) {
+	struct list_item_t* item=NULL;
+	struct list_head* cur_node=NULL;
+	list_for_each(cur_node, list) {
+	/* item points to the structure wherein the links are embedded */
+		item = list_entry(cur_node, struct list_item, links);
+		printf("%i \n", item->data);
+	}
+}
+
+void blah(void) {
+	INIT_LIST_HEAD(&my_list); /* Initialize the list */
+	populate(&my_list);	/* Populate the list */
+	print_list(&my_list);
 }
