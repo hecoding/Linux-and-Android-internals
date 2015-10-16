4ceb76afa6276a87d9e1359dccfa1fa869ee0b42
jvalhond1 <jvalhond@ucm.es>
2015-10-16 12:06:38 +0200

    Update modlist.c

--- modlist.c
+++ modlist.c
@@ -62,12 +62,83 @@ void exit_modlist_module( void )
 }
 
 
-static int modlist_read(/* parametros */) {
+static int modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    int nr_bytes = sizeof(int);
+    char *kbuf;
+    int cont = 0;
+    int num =0;
+    int tam =0;
+    list_item* item=NULL;
+    struct list_head* cur_node=NULL;
+    
+    if ((*off) > 0) /* Tell the application that there is nothing left to read */
+        return 0;
+      
+    if (len<nr_bytes)
+      return -ENOSPC;
 
+    list_for_each(cur_node, &mylist) {
+      /* item points to the structure wherein the links are embedded */
+      item = list_entry(cur_node, list_item, links);
+      num = item->data;
+      //trace_printk("Current value of clipboard: %i\n",num );
+      kbuf+= sprintf(kbuf,"%i\n",num);
+      trace_printk("Current value of clipboard: %s\n",kbuf );
+      cont++;
+      //printk(KERN_INFO "%i\n",item->data);
+      /* Transfer data from the kernel to userspace  */
+      
+    }
+    kbuf += '\0';
+    tam = strlen(kbuf);
+     if (copy_to_user(buf, kbuf,tam))
+        return -EINVAL;
+    
+      /* Transfer data from the kernel to userspace */
+    //if (copy_to_user(buf, clipboard,nr_bytes))
+     // return -EINVAL;
+      
+    (*off)+=len;  /* Update the file pointer */
+
+    return nr_bytes; 
 }
 
-static int modlist_write(/* parametros */) {
+static int modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+	  int available_space = BUFFER_LENGTH-1;
+    char kbuf[1000];
+    int num = 0;
+    
+    if ((*off) > 0) /* The application can write in this entry just once !! */
+      return 0;
+    
+    if (len > available_space) {
+      printk(KERN_INFO "modlist: not enough space!!\n");
+      return -ENOSPC;
+    }
 
+    
+    /* Transfer data from user to kernel space */
+    if (copy_from_user( &kbuf, buf, len ))  
+      return -EFAULT;
+
+    
+
+    if(sscanf(kbuf, "add %i", &num) == 1) {
+      list_item* nodo = vmalloc(sizeof(list_item));
+      
+      nodo->data = num;
+
+      trace_printk("Current list empty: %i\n",list_empty(&mylist) );
+
+      list_add_tail(&nodo->links,&mylist);
+      trace_printk("Current value of clipboard: %i\n",num );
+      trace_printk("Current list empty: %i\n",list_empty(&mylist) );
+  }
+
+  //clipboard[len] = '\0'; /* Add the `\0' */  
+  *off+=len;            /* Update the file pointer */
+  
+  return len;
 }
 
 static int modlist_add(/* parametros */) {
