f46bdbc4e22011b46b3af0de660acec3688a1358
hecoding <hlaria@ucm.es>
2015-10-22 01:39:13 +0200

    buffer unitario

--- modlist.c
+++ modlist.c
@@ -8,10 +8,9 @@
 
 MODULE_LICENSE("GPL");
 
-#define BUFFER_LENGTH PAGE_SIZE
+#define BUFFER_LENGTH 1024
 
 static struct proc_dir_entry *proc_entry;
-char* modlistbuffer;
 
 /* Nodos de la lista */
 typedef struct list_item {
@@ -36,7 +35,6 @@ static const struct file_operations proc_entry_fops = {
 int init_modlist_module( void )
 {
   int ret = 0;
-  modlistbuffer = (char *)vmalloc( BUFFER_LENGTH );
   INIT_LIST_HEAD( &mylist );
 
 
@@ -47,7 +45,6 @@ int init_modlist_module( void )
     proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
     if (proc_entry == NULL) {
       ret = -ENOMEM;
-      vfree(&mylist);
       printk(KERN_INFO "modlist: Can't create /proc entry\n");
     } else {
       printk(KERN_INFO "modlist: Module loaded\n");
@@ -63,12 +60,13 @@ void exit_modlist_module( void )
   remove_proc_entry("modlist", NULL);
   //vfree(&mylist); LA CABEZA NO ESTÁ EN MEMORIA DINÁMICA, CON HACER UN CLEANUP VALE
   modlist_cleanup(); // <- porque los demás nodos sí están en memoria dinámica pero mylist en la pila
-  vfree(modlistbuffer);
+  
   printk(KERN_INFO "modlist: Module unloaded.\n");
 }
 
 
 static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+  char* modlistbuffer = (char *)vmalloc( BUFFER_LENGTH );
   struct list_item* item=NULL;
   struct list_head* cur_node=NULL;
   ssize_t buf_length = 0;
@@ -91,11 +89,13 @@ static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, lof
     return -EINVAL;
     
   (*off)+=len;  /* Update the file pointer */
+  vfree(modlistbuffer);
 
   return buf_length;
 }
 
 static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+  char* modlistbuffer = (char *)vmalloc( BUFFER_LENGTH );
   int available_space = BUFFER_LENGTH-1;
   int num = 0;
   
@@ -129,6 +129,7 @@ static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t l
 
   modlistbuffer[len] = '\0'; /* Add the `\0' */  
   *off+=len;           /* Update the file pointer */
+  vfree(modlistbuffer);
 
   return len;
 }
