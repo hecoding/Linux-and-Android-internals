50a57a1151a6dc5ec08cf606b4f324c30a53bf91
jvalhond1 <jvalhond@ucm.es>
2015-11-19 20:12:30 +0100

    ParteB funciona

--- Pr2/ParteB/ledctl_invoke.c
+++ Pr2/ParteB/ledctl_invoke.c
@@ -2,14 +2,17 @@
 #include <sys/syscall.h>
 #include <linux/unistd.h>
 #include <stdio.h>
-#define __NR_gettid 316
+#include <stdlib.h>
+#define __NR_ledctl 316
 
-long ledctl(void) {
-	return (long) syscall(__NR_ledctl);
+long ledctl(unsigned int leds) {
+	return (long) syscall(__NR_ledctl, leds);
 }
 
-int main (void) {
-	printf("El código de retorno de la llamada ledctl es %ld\n",
-	ledctl());
+int main (int argc, char *argv[]) {
+	if(argc == 2)
+		ledctl(strtoul(argv[1], NULL, 16)); //strtoul convierte una cadena en un entero largo en base 16
+	else
+		printf("Usage: ./ledctl_invoke <ledmask>\n");
 	return 0;
 }
